2467b0be02f586d16750b21365b059b0e3186580
Ronel Cruz <rocruz@apap.com.do>
2022-04-05 12:59:24 -0400

    initial commit

---
+++ ccrd2/callcentersRD/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UnderconstructionComponent } from './components/underconstruction/underconstruction.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotFoundComponent,
+    HomeComponent,
+    NavComponent,
+    FooterComponent,
+    UnderconstructionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
